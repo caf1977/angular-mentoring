7dc6747d4849dca6ff40b69cd53497fe219931f8
Carlos Fuentes <carlos_fuentes@epam.com>
2025-04-21 10:24:21 -0500

    Adding implementation for Home Task

---
+++ src/app/models/course.interface.ts
@@ -0,0 +1,7 @@
+export interface Course {
+    id: string;            // Unique identifier for the course
+    title: string;         // Title of the course
+    creationDate: Date;    // Creation date of the course
+    duration: number;      // Duration of the course in minutes
+    description: string;
+}
---
+++ src/app/models/user.interface.ts
@@ -0,0 +1,5 @@
+export interface User {
+    id: string;         // Unique identifier for the user
+    firstName: string;  // First name of the user
+    lastName: string;   // Last name of the user
+  }
